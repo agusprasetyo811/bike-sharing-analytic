8d9919935abda5096386c7d8adf4d01fb2d60f97
Agus Prasetyo <agusprasetyo811@gmail.com>
2025-03-15 15:55:47 +0700

    init

---
+++ dashboard.py
@@ -0,0 +1,368 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+import scipy.stats as stats
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+sns.set(style='dark')
+
+# Inisialisasi session state untuk navigasi
+if "page" not in st.session_state:
+    st.session_state["page"] = "Home"
+
+# Fungsi untuk mengganti halaman
+def change_page(page):
+    st.session_state["page"] = page
+
+# Membaca Dataset
+days_df = pd.read_csv("day.csv")
+hours_df = pd.read_csv("hour.csv")
+days_df["cnt_log"] = np.log1p(days_df["cnt"])
+
+
+# st.header('Bike Sharing Dashboard')
+
+st.sidebar.title("Bike Sharing")
+page = st.sidebar.radio(
+	"Laporan Pengguna Sepeda", [
+    "Halaman Utama",
+		"Pengaruh Musim", 
+		"Pengaruh Cuaca",
+    "Hari Kerja vs Libur",
+    "Trend Bulanan",
+    "Trend Pengguna per Jam",
+    "Casual vs Registered User",
+    "Analisis Lanjutan"
+	]
+)
+
+if page == "Halaman Utama":
+  st.title("Bike Sharing")
+  st.write("Selamat datang di dashboard Bike Sharing!")
+  st.markdown(
+    "Dalam dashboard akan dilakukan analisis terhadap laporan penggunaan sepeda mulai dari beberapa kategori seperti\n"
+    "- Pengaruh Musim terhadap jumlah pengguna sepeda\n"
+    "- Pengaruh Cuaca terhadap jumlah pengguna sepeda\n"
+    "- Perbandingan jumlah pengguna di hari kerja dan hari libur\n"
+    "- Trend bulanan dari aktivitas pengguna sepeda\n"
+    "- Trend Pengguna per jam\n"
+    "- Perbandingan Casual vs Registered User\n\n"
+    "Pilih menu disamping untuk melihat hasil laporan"
+  )
+elif page == "Pengaruh Musim":
+	st.title("Pengaruh Musim terhadap jumlah pengguna sepeda")
+	
+	# Transformasi days_df khusus untuk season
+	season_labels = {
+			1: "spring",
+			2: "summer",
+			3: "fall",
+			4: "winter"
+	}
+	season_df = days_df.reset_index()
+	season_df.season =  season_df.season.map(season_labels)
+						
+	st.markdown("---")
+						
+	st.write("##### Berdasarkan Hasil Analisis ")
+	season_groups = [season_df[season_df["season"] == s]["cnt_log"] for s in season_df["season"].unique()]
+	anova_result = stats.f_oneway(*season_groups)
+	
+	if anova_result.pvalue < 0.05:st.write("Terdapat perbedaan signifikan jumlah pengguna antar season.")
+	else:st.write("Tidak ada perbedaan signifikan jumlah pengguna antar season.")
+						
+	st.markdown("---")
+	
+	# Menampilkan summary mean dalam table
+	season_summary = season_df.groupby(by="season").agg({
+		"cnt_log": "mean"                  
+	}).rename(columns={"index":"Season", "cnt_log": "Rata-rata Pengguna"}).rename_axis("Season")
+	st.write("##### Tabel perbandingan rata-rata pengguna berdasarkan season")
+	st.dataframe(season_summary)
+
+	# Visualisasi Season
+	st.write("##### Grafik perbandingan rata-rata pengguna berdasarkan season") 
+	fig, ax = plt.subplots(figsize=(35, 15))
+	sns.barplot(x="season", hue="season", y="cnt_log",  data=season_df, palette="Set2", estimator=np.mean, ax=ax)
+	ax.set_ylabel(None)
+	ax.set_xlabel("Season", fontsize=30)
+	ax.set_title("Rata-rata pengguna berdasarkan season", loc="center", fontsize=50, pad=30)
+	ax.tick_params(axis='y', labelsize=35)
+	ax.tick_params(axis='x', labelsize=30)
+	st.pyplot(fig)
+						
+	# Menampikan Uji Anova
+	st.write("##### Hasil uji statistik ANOVA")
+	st.write(f"P-Value ANOVA: {anova_result.pvalue}")
+
+	# Uji Statistic Post-Hoc Test dan tampilakan dalam bentuk table
+	season_tukey = pairwise_tukeyhsd(season_df["cnt_log"], season_df["season"])
+	season_tukey_df = pd.DataFrame(data=season_tukey.summary().data[1:], columns=season_tukey.summary().data[0])
+	st.write("##### Tabel perbandingan hasil tukey HSD Test")
+	st.dataframe(season_tukey_df)
+
+	st.write(
+		"Setelah dilakukan Uji ANOVA ternyata terdapat perbedaan yang signifikan bahwa season mempengaruhi jumlah pengguna.\n"
+		"Detail perbedaan yang signifikan terlihat jelas pada uji Post-Hoc Test dimana  p-aj rata-rata dibawah < 0.05 dan nilai reject true"
+	)   
+elif page == "Pengaruh Cuaca":
+	st.title("Pengaruh Cuaca terhadap jumlah pengguna sepeda")
+	
+	# Transformasi days_df khusus untuk season
+	weathersit_labels = {
+			1: "Clear",
+			2: "Mist",
+			3: "Light Snow",
+			4: "Heavy Rain"
+	}
+	weathersit_df = days_df.reset_index()
+	weathersit_df.weathersit =  weathersit_df.weathersit.map(weathersit_labels)
+						
+	st.markdown("---")
+	st.write("##### Berdasarkan Hasil Analisis ")
+	group1 = weathersit_df[weathersit_df["weathersit"] == 'Clear']["cnt_log"]
+	group2 = weathersit_df[weathersit_df["weathersit"] == 'Mist']["cnt_log"]
+	group3 = weathersit_df[weathersit_df["weathersit"] == 'Light Snow']["cnt_log"]
+	group4 = weathersit_df[weathersit_df["weathersit"] == 'Heavy Rain']["cnt_log"]
+
+	# Uji ANOVA
+	f_stat, p_value = stats.f_oneway(group1, group2, group3)
+	
+	if p_value < 0.05:st.write("Hasil signifikan: Cuaca berpengaruh terhadap jumlah pengguna sepeda.")
+	else:st.write("Hasil tidak signifikan: Cuaca mungkin tidak memiliki pengaruh yang besar.")
+	st.markdown("---")
+	
+	st.write("##### Grafik persebaran dampak cuaca terhadap pengguna sepeda")
+	fig, ax = plt.subplots(figsize=(35, 15))
+	sns.boxplot(x="weathersit", hue="weathersit", y="cnt_log", data=weathersit_df, palette="Set2", ax=ax)
+	ax.set_ylabel("Log Jumlah Pengguna Sepeda", fontsize=30)
+	ax.set_xlabel("Weathersit (Cuaca)", fontsize=30)
+	ax.set_title("Dampak Cuaca terhadap Jumlah Pengguna Sepeda", loc="center", fontsize=50, pad=30)
+	ax.tick_params(axis='y', labelsize=35)
+	ax.tick_params(axis='x', labelsize=30)
+	st.pyplot(fig)
+	
+	# Menampikan Uji Anova
+	st.write("##### Hasil uji statistik ANOVA")
+	st.write(f"F-statistic: {f_stat:.4f}, p-value: {p_value:.4f}")
+							
+	# Uji Statistic Post-Hoc Test dan tampilakan dalam bentuk table
+	weathersit_tukey = pairwise_tukeyhsd(endog=weathersit_df["cnt_log"], groups=weathersit_df["weathersit"], alpha=0.05)
+	weathersit_tukey_df = pd.DataFrame(data=weathersit_tukey.summary().data[1:], columns=weathersit_tukey.summary().data[0])
+	st.write("##### Tabel perbandingan hasil tukey HSD Test")
+	st.dataframe(weathersit_tukey_df)
+							
+	# Visualisasi weathersit tukey
+	group1 = weathersit_tukey_df["group1"].astype(str)
+	group2 = weathersit_tukey_df["group2"].astype(str)
+	mean_diff = weathersit_tukey_df["meandiff"]
+	p_values = weathersit_tukey_df["p-adj"]
+	colors = ["red" if p < 0.05 else "gray" for p in p_values]
+
+	fig, ax = plt.subplots(figsize=(35, 15))
+	ax.barh([f"{g1} vs {g2}" for g1, g2 in zip(group1, group2)], mean_diff, color=colors)
+	ax.axvline(x=0, color="black", linestyle="--", linewidth=1)
+	ax.set_ylabel("Perbandingan Weathersit", fontsize=30)
+	ax.set_xlabel("Perbedaan Rata-rata (Meandiff)", fontsize=30)
+	ax.set_title("Hasil Tukey HSD: Pengaruh Cuaca terhadap Pengguna Sepeda", loc="center", fontsize=50, pad=30)
+	ax.tick_params(axis='y', labelsize=35)
+	ax.tick_params(axis='x', labelsize=30)
+	st.pyplot(fig)
+							
+	st.write(
+		"Setelah dilakukan Uji ANOVA ternyata terdapat perbedaan yang signifikan bahwa weathersit mempengaruhi jumlah pengguna.\n"
+		"Detail perbedaan yang signifikan terlihat jelas pada uji Post-Hoc Test dimana  p-aj rata-rata dibawah < 0.05 dan nilai reject true.\n"
+		"Jika dilihat dari Tukey HSD terdapat perbedaan yang signifikan antar kedua kategori cuaca"
+	)
+elif page == "Hari Kerja vs Libur":
+	st.title("Perbandingan jumlah pengguna di hari kerja dan hari libur")
+	st.markdown("---")
+	st.write("##### Berdasarkan Hasil Analisis ")
+	st.write("Jumlah pengguna sepeda pada hari kerja dan hari biasa sangat dominan bila dibandingkan dengan hari libur / libur nasional")
+	st.markdown("---")
+	
+	fig, ax = plt.subplots(figsize=(35, 15))
+	
+	sns.histplot(days_df[days_df['workingday'] == 1]['cnt_log'], kde=True, color='blue', label='Hari Kerja', bins=30, alpha=0.6, ax=ax)
+	sns.histplot(days_df[days_df['workingday'] == 0]['cnt_log'], kde=True, color='red', label='Hari Libur', bins=30, alpha=0.6, ax=ax)
+	ax.set_ylabel("Frekuensi", fontsize=30)
+	ax.set_xlabel("Jumlah Pengguna Sepeda", fontsize=30)
+	ax.set_title("Distribusi Pengguna Sepeda: Hari Kerja vs Hari Libur", loc="center", fontsize=50, pad=30)
+	ax.tick_params(axis='y', labelsize=35)
+	ax.tick_params(axis='x', labelsize=30)
+	ax.legend(title="", fontsize=30, title_fontsize=30, loc="upper left")
+	st.pyplot(fig)
+	
+	fig, ax = plt.subplots(figsize=(35, 15))
+	sns.histplot(days_df[days_df['holiday'] == 1]['cnt_log'], kde=True, color='blue', label='Hari Libur Nasional', bins=30, alpha=0.6, ax=ax)
+	sns.histplot(days_df[days_df['holiday'] == 0]['cnt_log'], kde=True, color='red', label='Hari Biasa', bins=30, alpha=0.6, ax=ax)
+	ax.set_ylabel("Frekuensi", fontsize=30)
+	ax.set_xlabel("Jumlah Pengguna Sepeda", fontsize=30)
+	ax.set_title("Distribusi Pengguna Sepeda: Hari Libur Nasional vs Hari Biasa", loc="center", fontsize=50, pad=30)
+	ax.tick_params(axis='y', labelsize=35)
+	ax.tick_params(axis='x', labelsize=30)
+	ax.legend(title="", fontsize=30, title_fontsize=30, loc="upper left")
+	st.pyplot(fig)
+elif page == "Trend Bulanan":
+	st.title("Trend bulanan dari aktivitas pengguna sepeda")
+	st.markdown("---")
+	st.write("##### Berdasarkan Hasil Analisis ")
+	st.write("Pengguna meningkat dari awal tahun dan mencapai puncaknya pada bulan Mei - September")
+	st.markdown("---")
+							
+	fig, ax = plt.subplots(figsize=(35, 15))
+	monthly_trend = days_df.groupby("mnth")["cnt"].mean()
+	ax.plot(monthly_trend.index, monthly_trend.values, marker='o', linestyle='-', color='b')
+	ax.set_xticks(range(1, 13)) 
+	ax.set_xticklabels([
+		"Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+		"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+	])
+	ax.set_ylabel("Rata-rata Pengguna Harian", fontsize=30)
+	ax.set_xlabel("Bulan", fontsize=30)
+	ax.set_title("Tren Bulanan Penggunaan Sepeda", loc="center", fontsize=50, pad=30)
+	ax.tick_params(axis='y', labelsize=35)
+	ax.tick_params(axis='x', labelsize=30)
+	st.pyplot(fig)              
+elif page == "Trend Pengguna per Jam":
+		st.title("Trend Pengguna per jam")
+		st.markdown("---")
+		st.write("##### Berdasarkan Hasil Analisis ")
+		st.write("Trend Grafik menujukan beberapa pola seperti puncak pengguna terjadi di pagi dan sore hari dan pengguna rendah di malam dan dini hari")
+		st.markdown("---")
+                
+		hourly_trend = hours_df.groupby("hr")["cnt"].mean()
+                
+		fig, ax = plt.subplots(figsize=(35, 15))
+		ax.plot(hourly_trend.index, hourly_trend.values, marker='o', linestyle='-', color='b')
+		ax.set_ylabel("Jumlah Rata-rata Pengguna", fontsize=30)
+		ax.set_xlabel("Jam", fontsize=30)
+		ax.set_title("Tren Pengguna Per Jam", loc="center", fontsize=50, pad=30)
+		ax.tick_params(axis='y', labelsize=35)
+		ax.tick_params(axis='x', labelsize=30)
+		st.pyplot(fig)
+elif page == "Casual vs Registered User":
+	st.title("Perbandingan Casual vs Registered User")
+	st.markdown("---")
+	st.write("##### Berdasarkan Hasil Analisis ")
+	st.write(
+		"Pengguna casual lebih banyak diakhir pekan / hari libur dibanding hari kerja.\n"
+  	"Pengguna registered lebih banyak di hari kerja.\n"
+		"Pengguna casual lebih sensitif terhadap cuaca dibanding pengguna registered.\n"
+		"Pada saat cuaca buruk pengguna registered lebih stabil.\n"
+		"Jumlah pengguna casual meningkat pada suhu yang hangat dan cenderung turun pada suhu yang dingin dan kelembapan tinggi.\n"
+		"Jumlah pengguna registered terlihat mengalami penurunan juga namun cenderung lebih stabil\n"
+	)
+	st.markdown("---")
+
+	# Perbandingan casual & registered diambil dari mean workingday dan weathersit
+	grouped_weekday = days_df.groupby("workingday")[["casual", "registered"]].mean()
+	grouped_weather = days_df.groupby("weathersit")[["casual", "registered"]].mean()
+
+	fig, ax = plt.subplots(figsize=(35, 15))
+	sns.barplot(x=grouped_weekday.index, y=grouped_weekday["casual"], color="blue", label="Casual", ax=ax)
+	sns.barplot(x=grouped_weekday.index, y=grouped_weekday["registered"], color="red", alpha=0.7, label="Registered", ax=ax)
+	ax.set_xticks([0,1])
+	ax.set_xticklabels(["Weekend/Holiday", "Weekday"])
+	ax.set_ylabel("Rata-rata Jumlah Pengguna", fontsize=30)
+	ax.set_xlabel("Kategori Hari", fontsize=30)
+	ax.set_title("Perbandingan Pengguna Casual vs Registered pada Hari Kerja dan Akhir Pekan", loc="center", fontsize=50, pad=30)
+	ax.tick_params(axis='y', labelsize=35)
+	ax.tick_params(axis='x', labelsize=30)
+	ax.legend(title="", fontsize=30, title_fontsize=30, loc="upper left")
+	st.pyplot(fig) 
+  
+	fig, ax = plt.subplots(figsize=(35, 15))
+	sns.barplot(x=grouped_weather.index, y=grouped_weather["casual"], color="blue", label="Casual", ax=ax)
+	sns.barplot(x=grouped_weather.index, y=grouped_weather["registered"], color="red", alpha=0.7, label="Registered", ax=ax)
+	ax.set_xticks([0,1,2])
+	ax.set_xticklabels(["Cerah", "Mendung", "Hujan"])
+	ax.set_ylabel("Rata-rata Jumlah Pengguna", fontsize=30)
+	ax.set_xlabel("Kondisi Cuaca", fontsize=30)
+	ax.set_title("Pengaruh Kondisi Cuaca terhadap Pengguna Casual vs Registered", loc="center", fontsize=50, pad=30)
+	ax.tick_params(axis='y', labelsize=35)
+	ax.tick_params(axis='x', labelsize=30)
+	ax.legend(title="", fontsize=30, title_fontsize=30, loc="upper left")
+	st.pyplot(fig)
+	
+	fig, ax = plt.subplots(figsize=(35, 15))
+	sns.regplot(x=days_df["temp"], y=days_df["casual"], scatter_kws={"alpha":0.5}, label="Casual", color="blue", ax=ax)
+	sns.regplot(x=days_df["temp"], y=days_df["registered"], scatter_kws={"alpha":0.5}, label="Registered", color="red", ax=ax)
+	ax.set_ylabel("Temperatur (Normalisasi)", fontsize=30)
+	ax.set_xlabel("Jumlah Pengguna", fontsize=30)
+	ax.set_title("Pengaruh Suhu terhadap Pengguna Casual vs Registered", loc="center", fontsize=50, pad=30)
+	ax.tick_params(axis='y', labelsize=35)
+	ax.tick_params(axis='x', labelsize=30)
+	ax.legend(title="", fontsize=30, title_fontsize=30, loc="upper left")
+	st.pyplot(fig)
+ 
+	fig, ax = plt.subplots(figsize=(35, 15))
+	sns.regplot(x=days_df["hum"], y=days_df["casual"], scatter_kws={"alpha":0.5}, label="Casual", color="blue", ax=ax)
+	sns.regplot(x=days_df["hum"], y=days_df["registered"], scatter_kws={"alpha":0.5}, label="Registered", color="red", ax=ax)
+	ax.set_ylabel("Kelembaban (Normalisasi)", fontsize=30)
+	ax.set_xlabel("Jumlah Pengguna", fontsize=30)
+	ax.set_title("Pengaruh Kelembaban terhadap Pengguna Casual vs Registered", loc="center", fontsize=50, pad=30)
+	ax.tick_params(axis='y', labelsize=35)
+	ax.tick_params(axis='x', labelsize=30)
+	ax.legend(title="", fontsize=30, title_fontsize=30, loc="upper left")
+	st.pyplot(fig)
+elif page == "Analisis Lanjutan":
+  st.title("Analisis Lanjutan menggunakan metode Clustering")
+  st.markdown("---")
+  st.write("##### Berdasarkan Hasil Analisis ")
+  st.write(
+		"Cluster dengan banyak pengguna casual & sedikit registered Kemungkinan ini adalah pengguna yang hanya memakai sepeda sesekali (misalnya, di akhir pekan).\n"
+		"Cluster dengan banyak pengguna registered & sedikit casual mungkin pengguna tetap yang memakai sepeda untuk aktivitas harian seperti bekerja atau sekolah.\n"
+		"Cluster campuran dengan jumlah pengguna casual dan registered seimbang bisa jadi kelompok yang kadang memakai sepeda secara terdaftar, kadang tidak.\n"
+	)
+  st.write("##### Strategi mengubah pengguna casual menjadi pengguna registered ")
+  st.markdown(
+		"- Targetkan promo kepada pengguna di cluster casual agar mereka lebih tertarik menjadi pengguna registered.\n"
+		"- Berikan keuntungan eksklusif bagi pengguna registered seperti tarif lebih murah, akses jalur khusus, atau fitur tambahan.\n"
+	)
+  st.markdown("---")
+  
+  # Memilih fitur untuk clustering
+  features = ["casual", "registered", "temp", "hum", "windspeed", "workingday"]
+  X = days_df[features]
+  
+  # Standarisasi data
+  scaler = StandardScaler()
+  X_scaled = scaler.fit_transform(X)
+  
+	# Menentukan jumlah klaster optimal menggunakan metode Elbow
+  wcss = []
+  for k in range(1, 11):
+    kmeans = KMeans(n_clusters=k, random_state=42, n_init=10)
+    kmeans.fit(X_scaled)
+    wcss.append(kmeans.inertia_)
+    
+  fig, ax = plt.subplots(figsize=(35, 15))
+  ax.plot(range(1, 11), wcss, marker="o", linestyle="-", color="blue")
+  ax.set_ylabel("WCSS (Within-Cluster Sum of Squares)", fontsize=30)
+  ax.set_xlabel("Jumlah Klaster", fontsize=30)
+  ax.set_title("Metode Elbow untuk Menentukan Jumlah Klaster Optimal", loc="center", fontsize=50, pad=30)
+  ax.tick_params(axis='y', labelsize=35)
+  ax.tick_params(axis='x', labelsize=30)
+  st.pyplot(fig)
+  
+  # Menjalankan K-Means dengan 3 klaster
+  kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
+  days_df["cluster"] = kmeans.fit_predict(X_scaled)
+  
+  fig, ax = plt.subplots(figsize=(35, 15))
+  sns.scatterplot(x=days_df["casual"], y=days_df["registered"], hue=days_df["cluster"], palette="viridis", alpha=0.7, ax=ax)
+  ax.set_ylabel("Jumlah Pengguna Registered", fontsize=30)
+  ax.set_xlabel("Jumlah Pengguna Casual", fontsize=30)
+  ax.set_title("Hasil Clustering Pengguna Sepeda", loc="center", fontsize=50, pad=30)
+  ax.tick_params(axis='y', labelsize=35)
+  ax.tick_params(axis='x', labelsize=30)
+  ax.legend(title="Cluster", fontsize=30, title_fontsize=30, loc="upper left")
+  st.pyplot(fig)
+  
+
+st.caption('Copyright (c) Agus Prasetyo')
